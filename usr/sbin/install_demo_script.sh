5b8b3ab165b1d8f9e33964fb57dfbcd794636161
Joe Zhang <joe.zhang@technexion.com>
2023-12-25 11:57:27 +0800

    script: add demo script only on tek-orin/ VLS316

--- usr/sbin/install_demo_script.sh
+++ usr/sbin/install_demo_script.sh
@@ -4,8 +4,22 @@ echo '***********************************' > /dev/ttyTCU0
 echo 'Install Demo Script START!!!' > /dev/ttyTCU0
 echo '***********************************' > /dev/ttyTCU0
 
+TEK_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "tek-orin")
+TN316_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "vl316")
+
+if [ -z ${TEK_ORIN} ] && [ -z ${TN316_ORIN} ];then
+	echo '===================================' > /dev/ttyTCU0
+	echo 'This board have no 8 camera demo script.' > /dev/ttyTCU0
+	echo '===================================' > /dev/ttyTCU0
+	return 0
+fi
+
 DEMO_SCRIPT='/home/ubuntu/Desktop/TNDemo.desktop'
-SCRIPT_NAME=$(find /home/ubuntu/ -name "TechNexion_8_Cam_Demo_*.sh")
+if [[ ! -z ${TEK_ORIN} ]];then
+	SCRIPT_NAME=$(find /home/ubuntu/ -name "TechNexion_8_Cam_Demo_TEK.sh")
+elif [[ ! -z ${TN316_ORIN} ]];then
+	SCRIPT_NAME=$(find /home/ubuntu/ -name "TechNexion_8_Cam_Demo_EVK.sh")
+fi
 cp -rv /usr/share/applications/vizionviewer.desktop ${DEMO_SCRIPT}
 sed -i "$(grep -n "Name=" ${DEMO_SCRIPT} | cut -d ':' -f 1)s|Name=.*|Name=TechNexion 8 Camera demo|" ${DEMO_SCRIPT}
 sed -i "$(grep -n "Exec=" ${DEMO_SCRIPT} | cut -d ':' -f 1)s|Exec=.*|Exec=${SCRIPT_NAME}|" ${DEMO_SCRIPT}
